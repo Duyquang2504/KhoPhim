06f91b9fde644435e3341f0f14ab2241cb0f8295
Duyquang2504 <quang250412@gmail.com>
2025-12-13 06:11:16 +0700

    fix store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -10,3 +10,7 @@ export const store = configureStore({
     getDefaultMiddleware().concat(moviesApi.middleware),
 });
 setupListeners(store.dispatch);
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
